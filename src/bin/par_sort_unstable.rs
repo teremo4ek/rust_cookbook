6eea845b375cc17a4df8e1e6c59ddfb0ae2e7e8b
Yury Bely <teremo4ek@gmail.com>
2026-05-18 15:49:26 +0300

    Add rayon examples and resource files

---
+++ src/bin/par_sort_unstable.rs
@@ -0,0 +1,17 @@
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use rayon::prelude::*;
+
+fn main() {
+    let mut vec = vec![String::new(); 100];
+    vec.par_iter_mut().for_each(|p| {
+        let mut rng = thread_rng();
+
+        *p = (0..10)
+            .map(|_| rng.sample(&Alphanumeric))
+            .map(char::from)
+            .collect()
+    });
+    vec.par_sort_unstable();
+    println!("{:?}", vec);
+}
