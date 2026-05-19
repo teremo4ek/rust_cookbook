6d40e1dc1d2618f18c4485625b796463be5c2792
Yury Bely <teremo4ek@gmail.com>
2026-05-19 16:00:50 +0300

    Replace error_chain with anyhow in error_2, rename error1 to error_1

---
+++ src/bin/error_2.rs
@@ -0,0 +1,31 @@
+use anyhow::{anyhow, Context, Result};
+
+fn parse_response(response: reqwest::blocking::Response) -> Result<u32> {
+    let mut body = response.text()?;
+    body.pop();
+    body.parse::<u32>()
+        .with_context(|| anyhow!("Unexpected response: {}", body))
+}
+
+fn run() -> Result<()> {
+    let url =
+        format!("https://www.random.org/integers/?num=1&min=0&max=10&col=1&base=10&format=plain");
+    let response = reqwest::blocking::get(&url)?;
+    let random_value: u32 = parse_response(response)?;
+    println!("Random integer between 0 and 10: {}", random_value);
+    Ok(())
+}
+
+fn main() {
+    if let Err(error) = run() {
+        if let Some(_err) = error.downcast_ref::<std::io::Error>() {
+            println!("Standard I/O error: {:?}", error);
+        } else if let Some(_err) = error.downcast_ref::<reqwest::Error>() {
+            println!("Reqwest error: {:?}", error);
+        } else if let Some(_err) = error.downcast_ref::<std::num::ParseIntError>() {
+            println!("Parse int error: {:?}", error);
+        } else {
+            println!("Other error: {:?}", error);
+        }
+    }
+}
--- src/bin/log.rs
+++ src/bin/log.rs
@@ -17,7 +17,7 @@ fn main() {
         .filter(None, LevelFilter::Info)
         .init();
 
-    log::warn!("warn");
+    log::warn!("warning!!!");
     log::info!("info");
     log::debug!("debug");
 }
